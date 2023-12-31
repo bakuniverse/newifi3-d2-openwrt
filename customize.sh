bc435c59437f6a0908916eafe64691e03f21ff0b
bakuniverse <30513244+bakuniverse@users.noreply.github.com>
2023-12-31 19:54:25 +0800

    Create customize.sh

---
+++ customize.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: eSirPlayground
+# Youtube Channel: https://goo.gl/fvkdwm 
+#=================================================
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.100.1/g' openwrt/package/base-files/files/bin/config_generate
+
+#2. Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#3. Replace with JerryKuKu’s Argon
+#rm openwrt/package/lean/luci-theme-argon -rf
+#----------------------------------------------------------------------------#
+# If On Github Actions, remove bloat to get space (~ 30 GB)
+if [ "$USER" = "runner" ] || [ "$(whoami)" = "runner" ]; then
+   echo -e "\n[+] Debloating GH Runner...\n"
+     #12.0 GB
+     sudo rm /usr/local/lib/android -rf 2>/dev/null
+     #8.2 GB
+     sudo rm /opt/hostedtoolcache/CodeQL -rf 2>/dev/null
+     #5.0 GB
+     sudo rm /usr/local/.ghcup -rf 2>/dev/null
+     #2.0 GB
+     sudo rm /usr/share/dotnet -rf 2>/dev/null
+     #1.7 GB
+     sudo rm /usr/share/swift -rf 2>/dev/null
+     #1.1 GB
+     #sudo rm /usr/local/lib/node_modules -rf 2>/dev/null
+     #1.0 GB
+     sudo rm /usr/local/share/powershell -rf 2>/dev/null
+     #500 MB
+     sudo rm /usr/local/lib/heroku -rf 2>/dev/null
+fi
+#----------------------------------------------------------------------------#
