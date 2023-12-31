fb40a9a5c2268c53827e55c9a1b525e97086d7d7
bakuniverse <30513244+bakuniverse@users.noreply.github.com>
2023-12-31 14:46:12 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,15 +8,15 @@ sudo apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bis
   mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 python3-pyelftools \
   libpython3-dev qemu-utils rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip \
   vim wget xmlto xxd zlib1g-dev
-
+export OP_BUILD_PATH=$PWD
 git clone -b https://github.com/coolsnowwolf/lede.git openwrt
 
 #ln -sf /workdir/openwrt $GITHUB_WORKSPACE
 
-cd /workdir/openwrt || exit
+cd "${OP_BUILD_PATH}"/openwrt || exit
 ./scripts/feeds update -a && ./scripts/feeds install -a
 rm -rf ./tmp && rm -rf .config
-mv " $GITHUB_WORKSPACE"/.config " $GITHUB_WORKSPACE"/openwrt/.config
+mv "${OP_BUILD_PATH}"/.config "${OP_BUILD_PATH}"/openwrt/.config
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 make defconfig
 make download -j8
